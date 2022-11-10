ed3dc2b0b171ee33c852308886bb85e99975f589
Nikita Zhed <zhed-nikita@mail.ru>
2022-11-10 17:58:13 +0500

    add pageNumber + start RoomPage

--- work/side-menu.js
+++ work/side-menu.js
@@ -1,11 +1,13 @@
 const SideMenu = document.getElementsByClassName('side-menu')[0]
 const Nomera = document.getElementsByClassName('nomera')[0]
 const BtnAnimation = document.getElementsByClassName('menu-btn__animation')[0]
+const Page = document.getElementsByClassName('page__number')[0]
 const MenuBtn = document.getElementsByClassName('menu-btn')[0]
 
 
 MenuBtn.onclick = (event) => {
     SideMenu.classList.toggle('side-menu_active')
     Nomera.classList.toggle('nomera_active')
-    BtnAnimation.classList.toggle('menu-btn__animation_active')
+    BtnAnimation.classList.toggle('menu-btn__animation_active')    
+    Page.classList.toggle('page__number_active')
 }
