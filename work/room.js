b5fb57290b333a9f7db882d831f70591c972150f
Nikita Zhed <zhed-nikita@mail.ru>
2022-11-15 16:41:22 +0500

    add box to room

--- work/room.js
+++ work/room.js
@@ -1,17 +1,40 @@
-var LikeBox = document.getElementsByClassName('comment__like')[0]
-var LikeValue = document.getElementsByClassName('comment__value')[0]
+var LikeBox0 = document.getElementsByClassName('comment__like')[0]
+var LikeValue0 = document.getElementsByClassName('comment__value')[0]
 
 
-console.dir(LikeValue)
-
-LikeBox.onclick = event => {
-    if (LikeBox.classList == 'comment__like') {
-        LikeValue.innerHTML = Number(LikeValue.innerHTML) + 1
+LikeBox0.onclick = event => {
+    if (LikeBox0.classList == 'comment__like') {
+        LikeValue0.innerHTML = Number(LikeValue0.innerHTML) + 1
     }
     else {
-        LikeValue.innerHTML = Number(LikeValue.innerHTML) - 1
+        LikeValue0.innerHTML = Number(LikeValue0.innerHTML) - 1
     }
 
-    LikeBox.classList.toggle('comment__like_active')
-    LikeValue.classList.toggle('comment__value_active')
+    LikeBox0.classList.toggle('comment__like_active')
+    LikeValue0.classList.toggle('comment__value_active')
+}
+
+var LikeBox1 = document.getElementsByClassName('comment__like')[1]
+var LikeValue1 = document.getElementsByClassName('comment__value')[1]
+
+LikeBox1.onclick = event => {
+    if (LikeBox1.classList == 'comment__like') {
+        LikeValue1.innerHTML = Number(LikeValue1.innerHTML) + 1
+    }
+    else {
+        LikeValue1.innerHTML = Number(LikeValue1.innerHTML) - 1
+    }
+
+    LikeBox1.classList.toggle('comment__like_active')
+    LikeValue1.classList.toggle('comment__value_active')
+}
+
+var btn = document.getElementsByClassName('menu-btn')[0]
+var arrow = document.getElementsByClassName('btn-arrow')[0]
+var box = document.getElementsByClassName('box')[0]
+
+btn.onclick=event=>{
+    btn.classList.toggle('menu-btn_active')
+    arrow.classList.toggle('btn-arrow_active')
+    box.classList.toggle('box_active')
 }
