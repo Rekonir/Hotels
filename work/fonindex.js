a60cdf783ad7da6e2d39059ac0835510d741d439
Rekonir <zhed-nikita@mail.ru>
2023-06-02 11:54:41 +0300

    fix link of styles

---
+++ work/fonindex.js
@@ -0,0 +1,14 @@
+var f = document.getElementsByClassName('fon')[0]
+
+let i = 1
+let timerFon = setInterval(() => {
+    if (i == 1) {
+        f.style.backgroundImage = "url(./file/Fon2.png)"
+        i=i+1
+    }
+    else{
+        f.style.backgroundImage = "url(./file/Fon1.png)"
+        i=1
+    }
+    
+}, 5000)
